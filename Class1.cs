c8b7fd1600b4d91d5a0d5c35549dd1e457a7e7be
steve <steve@mulsanne.local>
2022-01-31 22:19:27 +0900

    プロジェクト ファイルを追加します。

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using HarmonyLib;
+using BepInEx;
+
+namespace TBMLogCommandLineOptions
+{
+    [BepInPlugin("jp.stevenson.timberbnmod.log_commandline_options", "log commandline options", "0.1")]
+    public class Class1 : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            new Harmony("jp.stevenson.timberbnmod.log_commandline_options").PatchAll();
+            Debug.Log("log commandline options : Loaded.");
+
+        }
+    }
+}
---
+++ patch_FeatureToggleService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using UnityEngine;
+using HarmonyLib;
+using Timberborn.FeatureToggleSystem;
+
+namespace TBMLogCommandLineOptions
+{
+    [HarmonyPatch(typeof(FeatureToggleService), "InitializeToggles")]
+    class patch_FeatureToggleService
+    {
+        /*
+         * オプション一覧をログ出力する。
+         * オプションはtimberborn.exeのコマンドライン引数で指定する。
+         * 例）Golemsの場合、-feature-Golems
+         * ※大文字小文字を区別する。
+         */
+        public static bool Prefix()
+        {
+            Debug.Log("commandline option state:");
+
+            // private staticメソッドの取得
+            MethodInfo _GetToggleFields = typeof(FeatureToggleService).GetMethod("GetToggleFields", BindingFlags.NonPublic | BindingFlags.Static);
+            if (_GetToggleFields == null)
+            {
+                Debug.LogError("failed to get GetToggleFields().");
+                return true;
+            }
+            MethodInfo _GetToggleState = typeof(FeatureToggleService).GetMethod("GetToggleState", BindingFlags.NonPublic | BindingFlags.Static);
+            if (_GetToggleState == null)
+            {
+                Debug.LogError("failed to get GetToggleState().");
+                return true;
+            }
+
+            IEnumerable<FieldInfo> fields = (IEnumerable<FieldInfo>)_GetToggleFields.Invoke(null, null);
+            if (fields == null)
+            {
+                Debug.LogError("failed to get fields.");
+                return true;
+            }
+
+            foreach (FieldInfo fieldInfo in fields)
+            {
+                string name = fieldInfo.Name;
+                bool toggleState = (bool)_GetToggleState.Invoke(null, new object[] { name });
+                if (toggleState)
+                {
+                    Debug.Log("    Feature " + name + ": on");
+                }
+                else
+                {
+                    Debug.Log("    Feature " + name + ": off");
+                }
+            }
+
+            return true;
+        }
+    }
+}
